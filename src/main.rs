f05128d106717e23019a413dc519ed6deee001da
robertjdominguez <rob.dominguez@hasura.io>
2023-06-13 16:50:41 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,10 @@ use regex::Regex;
 use std::error::Error;
 use chrono::Local;
 
-
-async fn query_algolia(parsed_query: &str) -> Result<(), Box<dyn Error>> {
+async fn query_algolia(parsed_query: &str) {
     // regex to remove https://hasura.io/docs/latest/ from parsed_query
     let re = Regex::new(r#"https://hasura.io/docs/latest/"#).unwrap();
     let parsed_query = re.replace_all(parsed_query, "");
-    print!("{}", parsed_query);
 
     // take parsed_query and brake it into words with %20 in between them
     let parsed_query = parsed_query.replace("/", "%20")
@@ -16,7 +14,6 @@ async fn query_algolia(parsed_query: &str) -> Result<(), Box<dyn Error>> {
 
     // query and request
     let query: String = format!("{{ \"params\": \"query={}\" }}", parsed_query);
-    println!("{}", query);
     let resp = reqwest::Client::new()
         .post("<https://NS6GBGYACO-dsn.algolia.net/1/indexes/hasura-graphql/query>")
         .header("X-Algolia-API-Key", "8f0f11e3241b59574c5dd32af09acdc8")
@@ -30,11 +27,15 @@ async fn query_algolia(parsed_query: &str) -> Result<(), Box<dyn Error>> {
     let url = re.captures(&body).unwrap().get(1).unwrap().as_str();
     println!("{}", url);
 
-    Ok(())
+    
+    return url;
+
+
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+
+async fn main() {
     // command line args
     let args: Vec<String> = std::env::args().collect();
     // today's date in MM-DD-YYYY format
@@ -53,7 +54,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
     for arg in args.iter().skip(1) {
         let _ = query_algolia(arg).await;
     }
-
-    // graceful
-    Ok(())
 }
